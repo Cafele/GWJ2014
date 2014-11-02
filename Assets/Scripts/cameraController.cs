782bfb938f2ae48a19d4c6b90fcf2818251b01e4
Cafele <cafele2004@hotmail.com>
2014-11-02 17:44:49 -0300

    matar player que cae
    incluido que la camara mate a los personajes que caen

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -4,17 +4,17 @@ using System.Collections;
 public class cameraController : MonoBehaviour {
 
     public GameObject cameraTarget; // object to look at or follow
-    public GameObject player; // player object for moving
+    //public GameObject player; // player object for moving
 
     public float smoothTime = 0.1f;    // time for dampen
     public bool cameraFollowX = true; // camera follows on horizontal
     public bool cameraFollowY = true; // camera follows on vertical
-    public bool cameraFollowHeight = true; // camera follow CameraTarget object height
-    public float cameraHeight = 2.5f; // height of camera adjustable
+    //public bool cameraFollowHeight = true; // camera follow CameraTarget object height
+    //public float cameraHeight = 2.5f; // height of camera adjustable
     Vector2 velocity; // speed of camera movement
 
     private Transform thisTransform; // camera Transform
-
+    private Vector3 lastPos = Vector3.zero;
     // Use this for initialization
     void Start()
     {
@@ -24,13 +24,16 @@ public class cameraController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (cameraTarget.transform.position.y < lastPos.y)
+            return;
+        lastPos = cameraTarget.transform.position;
         if (cameraFollowX)
         {
-            thisTransform.position = new Vector3(Mathf.SmoothDamp(thisTransform.position.x, cameraTarget.transform.position.x, ref velocity.x, smoothTime), thisTransform.position.y, thisTransform.position.z);
+            thisTransform.position = new Vector3(Mathf.SmoothDamp(thisTransform.position.x, lastPos.x, ref velocity.x, smoothTime), thisTransform.position.y, thisTransform.position.z);
         }
         if (cameraFollowY)
         {
-            thisTransform.position = new Vector3(thisTransform.position.x, Mathf.SmoothDamp(thisTransform.position.y, cameraTarget.transform.position.y, ref velocity.y, smoothTime), thisTransform.position.z);
+            thisTransform.position = new Vector3(thisTransform.position.x, Mathf.SmoothDamp(thisTransform.position.y, lastPos.y, ref velocity.y, smoothTime), thisTransform.position.z);
         }
     }
 }
